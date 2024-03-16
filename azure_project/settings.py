7f86f29352c70140372194d5fbeb15ccfda9a867
shakuneko <56393184+shakuneko@users.noreply.github.com>
2024-03-16 14:27:12 +0800

    fix:cors

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -45,6 +45,25 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',  # 允許從此網址發送的請求
 ]
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT'
+]
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
 ROOT_URLCONF = 'azure_project.urls'
 
 TEMPLATES = [
